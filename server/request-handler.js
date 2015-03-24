2f41d393e95b9f797c2ae7bd5d6de4d0dee10c2a
MKS-Student <mks.student@hackreactor.com>
2015-03-24 12:25:29 -0700

    Finished basic requirements

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,124 +1,88 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var url = require("url");
- var Data = function(){
-    this.results = [];
-  }
+var Data = function(){
+  this.results = [];
+}
+var data = new Data();
 
-  var data = new Data();
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log("Serving request type " + request.method + " for url " + request.url);
-  //console.log(url.parse(request.url));
 
-  // The outgoing status.
-  //var statusCode = 200;
-
-  // See the note below about CORS headers.
+  //----DEFAULT VALUES----
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  //headers['Content-Type'] = "text/plain";
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  //response.writeHead(statusCode, headers);
-
- 
+  var statusCode = 200;
+  var returnData = JSON.stringify({});
+  var headerMessage = '';
   headers['Content-Type'] = "application/json";
+
+  //URIs
   parsedURL = url.parse(request.url);
   splitURL = parsedURL.pathname.slice(1).split("/");
-  console.log(splitURL);
-  if(request.method === 'OPTIONS'){
-    headers['access-control-allow-origin'] = request.headers.origin || '*';
-    response.writeHead(204, "No content", headers)
-    response.end();
-  }
 
-  if(parsedURL.pathname === '/classes/messages'){
-    
-
-
-    if(request.method === 'POST'){
-      var body = '';
-      request.on('data', function(chunk){
-        body += chunk;
-      });
-      request.on('end', function(){   
-        data.results.push(JSON.parse(body));
-        response.writeHead(201, headers);
-        response.end();
-      })
-    }
-
-    if(request.method === 'GET'){
-      console.log(data);
-      response.writeHead(200, "Got it!", headers);
-      response.write(JSON.stringify(data));
-      response.end();
-    }
-  } else if (splitURL[0] === 'classes'){
-    var room = splitURL[1];
-    
-    var dataCopy = data.results.slice(0);
-    data.results = [];
-    for(var i = 0; i < dataCopy.length; i++){
-      if(dataCopy[i].roomname === room){
-        data.results.push(dataCopy[i]);
+  
+  var router = function(){
+    if(splitURL[0] === 'classes'){ 
+      if(request.method === 'OPTIONS'){
+        optionsRequest();
+      } else if (request.method === 'GET'){
+        getRequest();
+      } else if (request.method === 'POST'){
+        postRequest();
       }
+    } else {
+      failedRequest();
     }
-    response.writeHead(200, "Good", headers);
-    response.write(JSON.stringify(data));
-    response.end();
-  } else {
-    response.writeHead(404, "File not found", headers);
-    response.end();
+    //Call writeHead and response.end for every request
+    response.writeHead(statusCode, headerMessage, headers);
+    response.end(returnData);
   }
+
+  var optionsRequest = function(){
+    headers['access-control-allow-origin'] = request.headers.origin || '*';
+    statusCode = 204;
+    headerMessage = "Options request accepted";
+  }
+
+  var getRequest = function(){
+    statusCode = 200;
+    headerMessage = "GET request received";
+    returnData = JSON.stringify(data);
+  }
+
+  var postRequest = function(){
+    statusCode = 201;
+    headerMessage = "POST received, thanks bro!";
+    
+    var body = '';  
+
+    request.on('data', function(chunk){
+      body += chunk;
+    });
+
+    request.on('end', function(){
+      body = JSON.parse(body);
+      body.roomname = getRoom();
+      data.results.push(body);
+    })
+  }
+
+  var failedRequest = function(){
+    statusCode = 404;
+    headerMessage = "File not found";
+  }
+
+  var getRoom = function(){
+    if(splitURL === 'messages'){
+      return 'lobby';
+    } else {
+      return splitURL[1];
+    }
+  }
+
+  //Call router immediately on request
+  router();
 };
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   "access-control-allow-origin": '*',
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
